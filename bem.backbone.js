7ad757dbbf4f68d478e01eb168a801352e1ab4ca
piumosso <golikoffartem@gmail.com>
2012-03-25 17:26:52 +0400

    Backbone.View extension for bem manipulation

--- bem.backbone.js
+++ bem.backbone.js
@@ -1,15 +1,97 @@
-(function(){
+(function($){
     if (!Backbone || !Backbone.View) {
         return;
     }
 
+
+    /**
+     * Utilites
+     */
+    var error = (console && console.error) ? console.error : function(errorText){};
+    var elementModlDataAttribute = 'bemMods';
+    var getElementMods = function($element){
+        var elementMods = $element.data(elementModlDataAttribute);
+
+        if (!elementMods) {
+            elementMods = {};
+            $element.data(elementModlDataAttribute, elementMods);
+        }
+
+        return elementMods;
+    };
+    var setElementMod = function($element, modName, modValue){
+        var elementMods = getElementMods($element);
+        elementMods[modName] = modValue;
+        $element.data(elementModlDataAttribute, elementMods);
+    };
+
+
+    /**
+     * jQuery methods for setting and removing modificators
+     */
+    $.fn.setMod = function(){
+        if (!this.blockName && !this.elementName) {
+            error('.setMod() error');
+            return this;
+        }
+
+        if (arguments.length == 0 || arguments.length > 2) {
+            error('.setMod() takes one or two attributes');
+            return this;
+        }
+
+        var mods = {};
+        var elementClass = this.blockName + '__' + this.elementName;
+
+        if (arguments.length == 1) {
+            mods = arguments[0];
+        } else {
+            mods[arguments[0]] = arguments[1];
+        }
+
+        return this.each(function($element){
+            var elementMods = getElementMods($element);
+
+            for (var modName in mods) {
+                if (modName in elementMods) {
+                    $element.removeClass(elementClass + '_' + modName + '_' + elementMods[modName]);
+                }
+                $element.addClass(elementClass + '_' + modName + '_' + mods[modName]);
+                setElementMod($element, modName, mods[modName]);
+            }
+        });
+    };
+    $.fn.removeMod = function(modName){
+        if (!this.blockName && !this.elementName) {
+            error('.removeMod() error');
+            return this;
+        }
+
+        var elementClass = this.blockName + '__' + this.elementName;
+
+        return this.each(function($element){
+            var elementMods = getElementMods($element);
+
+            if (modName in elementMods) {
+                $element.removeClass(elementClass + '_' + modName + '_' + elementMods[modName]);
+            }
+        });
+    };
+
+
     Backbone.BemView = Backbone.View.extend({
         blockName: '',
 
-        element: function(elementName){},
+        element: function(elementName){
+            var $block = this.$el;
+            var elementSelector = self.blockName + '__' + elementName;
+            var $elements = $block.find(elementSelector);
 
-        mod: function(mod){},
+            // Set blockName and elementName
+            $elements.blockName = self.blockName;
+            $elements.elementName = elementName;
 
-        removeMod: function(mod){}
+            return $elements;
+        }
     });
-})();
+})(jQuery);
